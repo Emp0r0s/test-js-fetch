0e74f4ef9bc9c5bb41f132ab033acdc38ceab14e
Saurabh Jain <98027861+Emp0r0s@users.noreply.github.com>
2023-04-10 14:05:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const userDataDiv = document.getElementById("user-data");
 fetch("https://2qdymf1olb.execute-api.us-east-1.amazonaws.com/Dev")
   .then((response) => response.json())
   .then((data) => {
-    const user = data.results[0];
+    const user = data.body.results[0];
     userDataDiv.innerHTML = `
     <img src="${user.picture.large}"
     <div>
