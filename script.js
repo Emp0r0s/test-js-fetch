5523576b75260a5f2105afad99a952beb4449318
Saurabh Jain <98027861+Emp0r0s@users.noreply.github.com>
2023-04-10 12:08:42 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const userDataDiv = document.getElementById("user-data");
+
+fetch("https://2qdymf1olb.execute-api.us-east-1.amazonaws.com/Dev")
+  .then((response) => response.json())
+  .then((data) => {
+    const user = data.results[0];
+    userDataDiv.innerHTML = `
+    <img src="${user.picture.large}"
+    <h1>Name : ${user.name.first} ${user.name.last}</h1>
+    <p>Email: ${user.email}</p>
+    <p>Mobile: ${user.phone}</p>
+    <p>Age: ${user.dob.age}</p>
+    <p>Country: ${user.location.country}</p>
+    <p>State: ${user.location.state}</p>
+    <p>City: ${user.location.city}</p>
+    `;
+  });
