edd8ef9a2833e3149722051a85e41478c85dda9f
Saurabh Jain <98027861+Emp0r0s@users.noreply.github.com>
2023-04-10 12:11:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ fetch("https://2qdymf1olb.execute-api.us-east-1.amazonaws.com/Dev")
     const user = data.results[0];
     userDataDiv.innerHTML = `
     <img src="${user.picture.large}"
+    <div>
     <h1>Name : ${user.name.first} ${user.name.last}</h1>
     <p>Email: ${user.email}</p>
     <p>Mobile: ${user.phone}</p>
@@ -13,5 +14,6 @@ fetch("https://2qdymf1olb.execute-api.us-east-1.amazonaws.com/Dev")
     <p>Country: ${user.location.country}</p>
     <p>State: ${user.location.state}</p>
     <p>City: ${user.location.city}</p>
+    </div>
     `;
   });
